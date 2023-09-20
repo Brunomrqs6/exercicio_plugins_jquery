f29bd709290bf6483a985fa224d968bf40d97386
Bruno <yatosad@gmail.com>
2023-09-20 08:29:20 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    })
+
+    $('button').click(function(){
+        alert("VAaaaaaaaaaaaaaa")
+    })
+    $('#telefone').mask("(00) 00000-0000", {
+        placeholder:"(11) 99999-9999"
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder: "00000-000"
+    })
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, digite seu nome.',
+            email: 'Por favor, digite seu e-mail',
+            telefone: 'Por favor, digite seu telefone',
+            endereco: 'Por favor, digite seu endereco',
+            cep: 'Por favor, digite seu CEP',
+            cpf: 'Por favor, digite seu CPF'
+        }
+
+})
